9e0e75030a2c342a10af982399e680d768d7a6f0
m1guelten <footballmiguelten@gmail.com>
2022-09-18 17:08:47 +0300

    new init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
 (function () {
   const container = document.querySelector('#carousel');
   const slides = container.querySelectorAll('.slide');
-  const pauseBtn = container.querySelector('#pause');
-  const prevBtn = container.querySelector('#prev');
-  const nextBtn = container.querySelector('#next');
-  const indicatorsContainer = container.querySelector('#indicators-container');
-  const indicators = indicatorsContainer.querySelectorAll('.indicator');
+  // const pauseBtn = container.querySelector('#pause');
+  // const prevBtn = container.querySelector('#prev');
+  // const nextBtn = container.querySelector('#next');
+  // const indicatorsContainer = container.querySelector('#indicators-container');
+  // const indicators = indicatorsContainer.querySelectorAll('.indicator');
 
   const SLIDES_COUNT = slides.length;
   const CODE_LEFT_ARROW = 'ArrowLeft';
   const CODE_RIGHT_ARROW = 'ArrowRight';
   const CODE_SPACE = 'Space';
+  const FA_PLAY = '<i class="fa-regular fa-circle-play"></i>';
+  const FA_PAUSE = '<i class="fa-regular fa-circle-pause"></i>';
+  const FA_PREV = '<i class="fa-solid fa-chevron-left"></i>';
+  const FA_NEXT = '<i class="fa-solid fa-chevron-right"></i>';
 
   let isPlaying = true;
   let timerID = null;
@@ -19,6 +23,8 @@
   let swipeEndX = null;
 
   function gotoNth(n) {
+    const indContainer = container.querySelector('.indicators');
+    const indicators = indContainer.querySelectorAll('.indicator');
     slides[currentSlide].classList.toggle('active');
     indicators[currentSlide].classList.toggle('active');
     currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
@@ -32,14 +38,16 @@
     gotoNth(currentSlide - 1);
   }
   function pause() {
+    const pauseBtn = container.querySelector('#pause');
     isPlaying = false;
     clearInterval(timerID);
-    pauseBtn.innerHTML = 'Play';
+    pauseBtn.innerHTML = FA_PLAY;
   }
   function play() {
+    const pauseBtn = container.querySelector('#pause');
     isPlaying = true;
     timerID = setInterval(gotoNext, 2000);
-    pauseBtn.innerHTML = 'Pause';
+    pauseBtn.innerHTML = FA_PAUSE;
   }
 
   function pausePlay() {
@@ -78,6 +86,10 @@
     if (swipeEndX - swipeStartX > 100) next();
   }
   function initListeners() {
+    const pauseBtn = container.querySelector('#pause');
+    const prevBtn = container.querySelector('#prev');
+    const nextBtn = container.querySelector('#next');
+    const indicatorsContainer = container.querySelector('.indicators');
     pauseBtn.addEventListener('click', pausePlay);
     nextBtn.addEventListener('click', next);
     prevBtn.addEventListener('click', prev);
@@ -86,8 +98,34 @@
     container.addEventListener('touchend', swipeEnd);
     document.addEventListener('keydown', pressKey);
   }
-
+  function _initControls() {
+    const controls = document.createElement('div');
+    const PAUSE = `<span class="control control-pause" id="pause">${FA_PAUSE}</span>`;
+    const PREV = `<span class="control control-prev" id="prev">${FA_PREV}</span>`;
+    const NEXT = `<span class="control control-next" id="next">${FA_NEXT}</span>`;
+    controls.setAttribute('class', 'controls');
+    controls.innerHTML = PAUSE + PREV + NEXT;
+    container.append(controls);
+  }
+  function _initIndicators() {
+    const indicators = document.createElement('div');
+    indicators.setAttribute('class', 'indicators');
+    container.append(indicators);
+    for (let i = 0; i < SLIDES_COUNT; i++) {
+      const indicator = document.createElement('span');
+      indicator.setAttribute(
+        'class',
+        i === 0 ? 'indicator active' : 'indicator'
+      );
+      indicator.setAttribute('data-slide-to', i);
+      indicators.append(indicator);
+    }
+    // const indContainer = container.querySelector('.indicators');
+    // const indItems = indContainer.querySelectorAll('.indicator');
+  }
   function init() {
+    _initControls();
+    _initIndicators();
     initListeners();
     timerID = setInterval(gotoNext, 2000);
   }
