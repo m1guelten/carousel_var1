6fe1f38f4d79307285c80bb1b6df10b402a31a61
m1guelten <footballmiguelten@gmail.com>
2022-09-18 23:52:35 +0300

    delete comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,6 @@
 (function () {
   const container = document.querySelector('#carousel');
   const slides = container.querySelectorAll('.slide');
-  // const pauseBtn = container.querySelector('#pause');
-  // const prevBtn = container.querySelector('#prev');
-  // const nextBtn = container.querySelector('#next');
-  // const indicatorsContainer = container.querySelector('#indicators-container');
-  // const indicators = indicatorsContainer.querySelectorAll('.indicator');
 
   const SLIDES_COUNT = slides.length;
   const CODE_LEFT_ARROW = 'ArrowLeft';
@@ -21,9 +16,12 @@
   let currentSlide = 0;
   let swipeStartX = null;
   let swipeEndX = null;
+  let pauseBtn = null;
+  let prevBtn = null;
+  let nextBtn = null;
+  let indContainer = null;
 
   function gotoNth(n) {
-    const indContainer = container.querySelector('.indicators');
     const indicators = indContainer.querySelectorAll('.indicator');
     slides[currentSlide].classList.toggle('active');
     indicators[currentSlide].classList.toggle('active');
@@ -38,13 +36,11 @@
     gotoNth(currentSlide - 1);
   }
   function pause() {
-    const pauseBtn = container.querySelector('#pause');
     isPlaying = false;
     clearInterval(timerID);
     pauseBtn.innerHTML = FA_PLAY;
   }
   function play() {
-    const pauseBtn = container.querySelector('#pause');
     isPlaying = true;
     timerID = setInterval(gotoNext, 2000);
     pauseBtn.innerHTML = FA_PAUSE;
@@ -86,14 +82,10 @@
     if (swipeEndX - swipeStartX > 100) next();
   }
   function initListeners() {
-    const pauseBtn = container.querySelector('#pause');
-    const prevBtn = container.querySelector('#prev');
-    const nextBtn = container.querySelector('#next');
-    const indicatorsContainer = container.querySelector('.indicators');
     pauseBtn.addEventListener('click', pausePlay);
     nextBtn.addEventListener('click', next);
     prevBtn.addEventListener('click', prev);
-    indicatorsContainer.addEventListener('click', gotoInd);
+    indContainer.addEventListener('click', gotoInd);
     container.addEventListener('touchstart', swipeStart);
     container.addEventListener('touchend', swipeEnd);
     document.addEventListener('keydown', pressKey);
@@ -106,6 +98,9 @@
     controls.setAttribute('class', 'controls');
     controls.innerHTML = PAUSE + PREV + NEXT;
     container.append(controls);
+    prevBtn = container.querySelector('#prev');
+    nextBtn = container.querySelector('#next');
+    pauseBtn = container.querySelector('#pause');
   }
   function _initIndicators() {
     const indicators = document.createElement('div');
@@ -120,8 +115,7 @@
       indicator.setAttribute('data-slide-to', i);
       indicators.append(indicator);
     }
-    // const indContainer = container.querySelector('.indicators');
-    // const indItems = indContainer.querySelectorAll('.indicator');
+    indContainer = container.querySelector('.indicators');
   }
   function init() {
     _initControls();
